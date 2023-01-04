bbc94a494a78f34f9da5cf123dd3b5d27e5c3f63
Harteos7 <harteos14@gmail.com>
2023-01-04 16:55:44 +0100

    version test read ok

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ ANGLAIS
 an application to read and write exel in php (and vice versa)
 
 be careful with :
+
 Do not change the name of the exel file! (or change it also in the code)
+
 Do not use the application if the exel file is open (the php code will not be able to access the exel file)
 
-
+It is also necessary that we do not leave holes in the applications to fill. The application extracts the data from exel by column until it encounters empty spaces where it will move on to the next column.
 
 
 FRANCAIS
@@ -15,5 +17,9 @@ FRANCAIS
 Une application pour lire et écrire du exel en php (et vice versa)
 
 Faire attention à :
+
 Ne pas changer le nom du fichier exel ! (ou le changer aussi dans le code)
+
 Ne pas utiliser l'application si le ficher exel est ouvert (le code php ne pourra pas accéderau fichier exel)
+
+Il faut aussi que l'on ne laisse pas de trous dans les applications renseigner. Pour ce où on ne peux pas les remplies car on ne sais pas on marque : 'pas encore fais' (pars exemple le test de cibersécurité).L'application extrait les donnée de exel par colonne jusqu'à rencontrer des casses vides où elle passera à la colonne suivante
--- controller.php
+++ controller.php
@@ -9,29 +9,44 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load("hello world.xlsx");
 $sheet = $spreadsheet->getActiveSheet();
 
+//it's the code for writing data in file.xlsx
 //change it
 $sheet->setCellValue('A1', 'Noms des applications');
 $sheet->setCellValue('A2', 'test');
 $sheet->setCellValue('A3', 'rien');
 $sheet->setCellValue('A4', 'cest pas A3');
-
-//take it
-$b = 'A1';
-$a1 = $sheet->getCell($b);
-$a2 = $sheet->getCell('A2');
-$a3 = $sheet->getCell('A3');
-$a4 = $sheet->getCell('A4');
-$a5 = $sheet->getCell('A5');
+$sheet->setCellValue('B1', 'Prix');
+$sheet->setCellValue('B2', '19745');
+$sheet->setCellValue('B3', '1475621');
+$sheet->setCellValue('B4', '155');
+$sheet->setCellValue('B5', '834548');
+$sheet->setCellValue('C1', 'Lieu');
+$sheet->setCellValue('C2', 'Lion');
+$sheet->setCellValue('C3', 'Lile');
 
 //write it again to Filesystem with the same name (=replace)
 $writer = new Xlsx($spreadsheet);
 $writer->save('hello world.xlsx');
 
-$arr = array(
-    'A1' => strval($a1), 
-    'A2' => strval($a2), 
-    'A3' => strval($a3), 
-    'A4' => strval($a4),
-    'A5' => strval($a5)
-);
+// it's the code for read data in file.xlsx
+$arr = array();
+$c = 'A';
+for ($i = 1; ; $i++) { // $i and $c are the coordinates, respectively the number is the letter (A1, A2, B2, C3, ...)
+
+    $b = strval($c).strval($i); // $b is the coordinates of the exel box
+    $a = $sheet->getCell($b); // $a is the content of the exel box
+    if ($a == '') { // we check that the new cell has data otherwise we change the column
+        $c++;
+        $i = 1;
+        $b = strval($c).strval($i);
+        $a = $sheet->getCell($b);
+
+            if ($a == '') {break;} // we check if the first cell of the new column has data if not, we stop the for (break)
+            else
+            $arr[strval($b)] = strval($a);
+        
+    } 
+    else
+    $arr[strval($b)] = strval($a); // we put everything in array (the key is the coordinates and the value of their data)
+}
 print_r ($arr);
