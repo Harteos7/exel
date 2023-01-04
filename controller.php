89fb9e7380a7d30f6ab219b194ee1afef3413b73
Harteos7 <harteos14@gmail.com>
2023-01-04 15:31:18 +0100

    version test

--- controller.php
+++ controller.php
@@ -16,14 +16,22 @@ $sheet->setCellValue('A3', 'rien');
 $sheet->setCellValue('A4', 'cest pas A3');
 
 //take it
+$b = 'A1';
+$a1 = $sheet->getCell($b);
 $a2 = $sheet->getCell('A2');
 $a3 = $sheet->getCell('A3');
 $a4 = $sheet->getCell('A4');
+$a5 = $sheet->getCell('A5');
 
 //write it again to Filesystem with the same name (=replace)
 $writer = new Xlsx($spreadsheet);
 $writer->save('hello world.xlsx');
 
-echo "A2 is $a2 A3 is $a3 A4 is $a4";
-
-
+$arr = array(
+    'A1' => strval($a1), 
+    'A2' => strval($a2), 
+    'A3' => strval($a3), 
+    'A4' => strval($a4),
+    'A5' => strval($a5)
+);
+print_r ($arr);
